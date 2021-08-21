060234cae726d9b2ff84586f75ee93b6fe246e7b
Robi Nurhidayat <robivanpersie290899@gmail.com>
2021-08-21 12:55:48 +0700

    create file read_data.py

--- read_data.py
+++ read_data.py
@@ -3,3 +3,9 @@ import pandas as pd
 dataku = pd.read_csv('./data/kapal_titanic.csv')
 
 print(dataku.head(3))
+
+print(dataku.info())
+
+print(dataku.describe())
+
+print(dataku.describe(include='O'))

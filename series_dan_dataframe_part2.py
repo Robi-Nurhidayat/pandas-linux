56ea8d40c250ed7dbe8863f69ee445f41937a4d0
Robi Nurhidayat <robivanpersie290899@gmail.com>
2021-08-22 20:18:52 +0700

    belajar membuat dataframe

---
+++ series_dan_dataframe_part2.py
@@ -0,0 +1,62 @@
+import numpy as np
+import pandas as pd
+
+# belajar membuat dataframe dari nol
+
+np.random.seed(100)
+
+dataku = pd.DataFrame(np.random.randn(3,4),['A','B','C'],['Satu','Dua','Tiga','Empat'])
+
+print(dataku)
+
+# menambahkan column baru
+
+dataku['data_baru'] = dataku['Empat'] + dataku['Tiga']
+
+print(dataku)
+
+
+# menghapus column
+# jika menggunakan cara dibawah ini harus ditampung kedalam variable dulu agar dihapus secara permanen
+
+print(dataku.drop('Dua',axis=1))
+
+
+# jika tidak ingin ditampung di dalam variabel
+# maka gunakan inplace = True
+
+dataku.drop('Satu', axis=1, inplace=True)
+
+print(dataku)
+
+# menghapus baris,, tinggal ganti axis nya jadi = 0
+
+dataku.drop('A', axis=0, inplace=True)
+
+print(dataku)
+
+print(dataku.shape)
+
+
+# melakukan proses komparasi
+
+dataku_bol = dataku > 0
+
+print(dataku_bol)
+
+print(dataku[dataku_bol])
+
+dataku = dataku[dataku > 0]
+
+print(dataku)
+
+
+dataku2 = pd.DataFrame(np.random.randn(3,4),['A','B','C'],['Satu','Dua','Tiga','Empat'])
+
+print(dataku2, '\n\n')
+
+hasil = dataku2[dataku2['Satu'] > 0]
+hasil2 = dataku2['Satu'] > 0
+print(hasil, '\n\n')
+
+print(hasil2)
